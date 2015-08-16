9b0e58804e9a623d4785839b198b317eb6b031a1
Petr Gotthard <petr.gotthard@honeywell.com>
2015-08-16 18:42:20 +0200

    Switch to latest gen_coap.

--- package.mk
+++ package.mk
@@ -2,8 +2,19 @@ RELEASABLE=true
 APP_NAME:=gen_coap
 
 UPSTREAM_GIT:=https://github.com/gotthardp/gen_coap.git
-UPSTREAM_REVISION:=159530740cff987c60d3898879774d28045495c8
+UPSTREAM_REVISION:=2334a08a89f0c8373a45ea28c647a456b609a8df
 RETAIN_ORIGINAL_VERSION:=true
 
 ORIGINAL_APP_FILE:=$(CLONE_DIR)/src/gen_coap.app.src
 DO_NOT_GENERATE_APP_FILE=true
+
+# add auto-generated files
+EBIN_BEAMS+=$(EBIN_DIR)/core_link_scanner.beam $(EBIN_DIR)/core_link_parser.beam
+
+define package_rules
+%.erl : %.xrl
+	$(ERLC) -o $$(@D) $$<
+
+%.erl : %.yrl
+	$(ERLC) -o $$(@D) $$<
+endef
